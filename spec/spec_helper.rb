3de184bf186860ecc1b1dc721ca570591c6a9f69
Els <Emathivannan@spartaglobal.com>
2018-09-27 10:39:29 +0100

    first commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+# Prints out the name of each test
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+
+# checks how much of the code has been tested
+at_exit { ChefSpec::Coverage.report! }
